0fe0825a772ca1544cff993c585908ca57b876f0
Ricardo Ramirez Hintze <ramirezhintze@gmail.com>
2018-11-27 11:04:16 -0600

    added gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Compile Sass
 gulp.task('sass', function(){
   return gulp.src(['src/scss/*.scss'])
     .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream())
 });
 
 // Watch & Serve
 gulp.task('serve', ['sass'], function(){
-
   browserSync.init({
     server: './src'
   });
